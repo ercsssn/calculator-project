3d43a8fbbc1fe8bae7f2aaa167ca77ac1854015f
ercsssn <ercsssn@gmail.com>
2022-05-08 00:31:35 +0800

    Declutter code
    Removed all operator functions and left add and operate functions just to create the logic. I also removed the if else condition and replaced the default value of display variable to an empty string.

--- script.js
+++ script.js
@@ -1,86 +1,27 @@
 // Basic Math Operators
-
 function add(num1, num2) {
+  
 	return num1 + num2;
 };
 
-function subtract(num1, num2) {
-	return num1 - num2;
-};
-
-function total(...digits) {
-  numbers = digits[0];
-  let total = numbers.reduce((result, number) => {
-    return result + number;
-  }, 0);
-  return total;
-	
-};
-
-function multiply(...digits) {
-  numbers = digits[0];
-  let product = numbers.reduce((result, number) => {
-    return result * number;
-  }, 1);
-  return product;
-};
-
-function power(num1 , num2) {
-	return num1 ** num2;
-};
-
-function factorial(num) {
-  
-  if (num == 0) {
-    return 1;
-  }
-
-  const arr = [];
-	for (let i = 1 ; i <= num ; i++) {
-    arr.push(i);
-  }
-
-  let total = arr.reduce((result, number) => {
-    return result * number;
-  }, 1);
-  return total;
-};
-
-
 // Operate Function
-
 function operate(operation,num1,num2) {
     
     return operation(num1,num2);
 };
 
-function selectKey(e) {
-  let input;
-  if (e.target !== e.currentTarget) {
-      button = e.target;
-
-      let value = button.textContent;
-      input = value;
-  }
-
-  if (display === undefined) {
-    display = input;
-  }else {
-    display = display + input;
-  }
-
-  displayInput();
-  
-}
-
 //Display Input
-let display;
+let display = '';
 function displayInput() {
   let screen = document.querySelector('.screen');
   screen.textContent = display;
 }
 
+//Add Event Listener to buttons
 const buttons = document.querySelectorAll('.buttons .nm-btn');
 buttons.forEach(button => button.addEventListener('click', function () {
-  selectKey(button);
+  
+  display += button.textContent;
+  
+  displayInput();
 }));
